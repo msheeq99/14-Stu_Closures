7793321da9b826389e7cd9a214924537005a2540
Mohamud <msheeq99@outlook.com>
2023-01-24 19:16:08 +0000

    counter function completed and returned the inner fuction

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // TODO: Complete the 'counter' function below.
-function counter() {}
+function counter() {
+    let count = 0;
+
+  // Return an inner function creating a closure.
+  return {
+    increment: function () {
+      // Increment the outer function's 'count' variable by one.
+      return ++count;
+    },
+  };
+}
+
+
 
 module.exports = counter;
